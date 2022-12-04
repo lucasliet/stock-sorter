6aa0921aeb7ac7950d4edd21bac232f1fdc4c238
lucasliet <lucasouliveira@gmail.com>
2022-12-03 23:35:27 -0300

    add sass, remove axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { v4 as uuid } from 'uuid';
 import { useEffect, useState } from 'react';
 
@@ -8,15 +7,16 @@ function App() {
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
-    axios.get('https://fundamentus-parser-86qx4n52wqvg.deno.dev/')
-      .then(res => setStocks(res.data));
+   fetch('https://fundamentus-parser.deno.dev/')
+      .then(r => r.json())
+      .then(r => setStocks(r));
   }, []);
 
   return (
     <div className="App">
       <div className="table-container">
         {stocks.length > 0 && (
-          <table className='table is-striped is-bordered is-hoverable'>
+          <table className='table is-striped is-bordered is-hoverable is-narrow'>
             <thead>
               {Object.keys(stocks[0]).map((key: string) => (
                 <th key={uuid()}>{key}</th>
@@ -24,7 +24,11 @@ function App() {
             </thead>
             <tbody>
               {stocks.map((stock: any) => (
-                <tr key={uuid()}>
+                <tr 
+                  key={uuid()} 
+                  onClick={() => {window.location.href = `https://fundamentus.com.br/detalhes.php?papel=${Object.values(stock)[0]}`}}
+                  className='is-clickable'
+                >
                   {Object.values(stock).map((value: any) => (
                     <td key={uuid()}>{value}</td>
                   ))}
