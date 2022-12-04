420f4536ccd87c2f97354066cb2f3f39ac8a9f18
lucasliet <lucasouliveira@gmail.com>
2022-12-03 23:31:09 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/stock-sorter/',
+  plugins: [react()],
+  server: {
+    open: true
+  },
+  build: { target: 'esnext' }
 })
